d0bd3a9df70cd73ddad4bc8469507adc27badcaa
kingkc082702 <kingkc082702@gmail.com>
2024-06-22 10:15:43 +0800

    Configured MySQL pool.

---
+++ src/config/pool.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2'
+
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.P_PORT,
+    waitForConnections: process.env.WAIT_FOR_CONNECTIONS,
+    connectionLimit: process.env.CONNECTION_LIMIT,
+    queueLimit: process.env.QUEUE_LIMIT
+})
+
+export default pool;
